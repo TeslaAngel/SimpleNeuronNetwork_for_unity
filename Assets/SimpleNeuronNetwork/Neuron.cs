01ef8007611ce10144b3e19b0409e53d710257d3
TeslaAngel <mohadot@outlook.com>
2025-06-16 13:39:44 -0700

    Neuron basics and FeedForward

--- Assets/SimpleNeuronNetwork/Neuron.cs
+++ Assets/SimpleNeuronNetwork/Neuron.cs
@@ -6,7 +6,7 @@ namespace SimpleNeuronNetwork
 {
     public class Neuron
     {
-        private uint inputNumber;
+        private int inputNumber;
         private float[] weights;
         private float bias;
 
@@ -14,9 +14,9 @@ namespace SimpleNeuronNetwork
         public delegate float ActivationFunction(float input);
         private ActivationFunction activationFunction;
 
-        public Neuron(uint inputNumber, float[] weights, float bias)
+        public Neuron(float[] weights, float bias)
         {
-            this.inputNumber = inputNumber;
+            inputNumber = weights.Length;
             this.weights = weights;
             this.bias = bias;
 
@@ -41,6 +41,36 @@ namespace SimpleNeuronNetwork
         {
             activationFunction = newActivationFunc;
         }
+
+        /// <summary>
+        /// Passing inputs forward to get an output
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        public float FeedForward(float[] inputs)
+        {
+            // input number have to match number of weight
+            if(inputs.Length != inputNumber)
+            {
+                Debug.Log("input number does not match the number of weight");
+                return -1f;
+            }
+
+            float sum = 0;
+            
+            // perform dot product on every input and their weights
+            for(int i = 0; i < inputNumber; i++)
+            {
+                sum += weights[i] * inputs[i];
+            }
+
+            // count in bias
+            sum += bias;
+
+            return sum;
+        }
+
+
     }
     
 }
